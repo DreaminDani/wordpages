337148e8c8d4277a78f2a7704cb4ea4fa57d495a
Daniel Sandoval <daniel@desandoval.net>
2022-08-02 17:38:57 -0600

    make it pretty

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -33,19 +33,55 @@
 		{/each}
 	</div>
 {:else}
-	<h1>Word Pages</h1>
-	<p>Make a bunch of pages with words on them (24 words per page)</p>
-	<small>Enter a list of words with a newline between each word...</small>
-	<br />
-	<textarea bind:value={words} name="words" cols="20" rows="20" />
-	<br />
-	<input type="checkbox" name="isRandom" bind:checked={isRandom} />
-	<label for="isRandom">Print words in random order</label><br />
-	<p>After you click "Submit", print in landscape on a letter sized paper without any margins</p>
-	<button on:click={renderWords}>Submit</button>
+	<div class="instructions">
+		<div class="left-side">
+			<h1>Word Pages</h1>
+			<p>Make a bunch of pages with words on them (24 words per page)</p>
+			<small>Enter a list of words with a newline between each word...</small>
+			<br />
+			<textarea bind:value={words} name="words" cols="20" rows="20" />
+			<br />
+			<input type="checkbox" name="isRandom" bind:checked={isRandom} />
+			<label for="isRandom">Print words in random order</label><br />
+			<button class="submit-button" on:click={renderWords}>Submit</button>
+		</div>
+		<div class="right-side">
+			<p>
+				After you click "Submit", use the browser to print in landscape on a letter sized paper
+				without any margins
+			</p>
+			<img
+				src="print-instructions.png"
+				alt="a screenshot showing how to set landscape and margins for a printer in chrome"
+				class="right-image"
+			/>
+		</div>
+	</div>
 {/if}
 
 <style>
+	.left-side {
+		width: 600px;
+	}
+	.right-side {
+		width: 500px;
+	}
+	.instructions {
+		display: flex;
+		justify-content: space-between;
+		flex-wrap: wrap;
+		align-items: flex-end;
+		margin-right: 24px;
+	}
+	.right-image {
+		width: 100%;
+		height: auto;
+	}
+	.submit-button {
+		margin-top: 0.5em;
+		font-size: 2em;
+	}
+
 	.container {
 		display: grid;
 		grid-template-columns: 1fr 1fr 1fr 1fr;
