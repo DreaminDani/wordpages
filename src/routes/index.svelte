b676bd34759862af33bffe6771ce75812f6ac3d3
Daniel Sandoval <daniel@desandoval.net>
2022-08-02 13:38:30 -0600

    first attempt

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let rendered = false;
+	let words = '';
+	let wordList = [''];
+
+	function renderWords() {
+		wordList = words.split('\n');
+		console.log(wordList);
+		rendered = true;
+	}
+</script>
+
+<svelte:head><title>Word Pages</title></svelte:head>
+
+{#if rendered}
+	{#each wordList as word, i}
+		{#if (i + 1) % 4 === 0}
+			<span>{word}</span>
+			<br />
+		{:else}
+			<span>{word}</span>
+		{/if}
+		{#if (i + 1) % 20 === 0}
+			<div class="pagebreak" />
+		{/if}
+	{/each}
+{:else}
+	<h1>Word Pages</h1>
+	<p>Make a bunch of pages with words on them (20 words per page)</p>
+	<small>Enter a list of words with a newline between each word...</small>
+	<br />
+	<textarea bind:value={words} name="words" cols="20" rows="20" />
+	<br />
+	<button on:click={renderWords}>Submit</button>
+{/if}
+
+<style>
+	span {
+		display: inline-block;
+		font-size: 40px;
+		margin-right: 20px;
+		margin-top: 20px;
+	}
+
+	@media print {
+		.pagebreak {
+			page-break-before: always;
+		}
+	}
+</style>
